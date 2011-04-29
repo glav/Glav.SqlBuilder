f8207d942ae27077ebd09abbe28d759db3fa81e8
Paul Glavich <glav@theglavs.com>
2011-04-30 09:22:11 +1000

    Fix for empty supporting database names

--- Glav.SQLBuilder/Builders/DBCreationBuildStep.cs
+++ Glav.SQLBuilder/Builders/DBCreationBuildStep.cs
@@ -10,91 +10,94 @@ using Glav.SQLBuilder.Helpers;
 
 namespace Glav.SQLBuilder.Builders
 {
-    public class DBCreationBuildStep : BuildStepBase
-    {
-        public DBCreationBuildStep(ILogger logger, IConfig config, ISqlServer databaseServer) : base(config,logger, databaseServer){}
+	public class DBCreationBuildStep : BuildStepBase
+	{
+		public DBCreationBuildStep(ILogger logger, IConfig config, ISqlServer databaseServer) : base(config, logger, databaseServer) { }
 
-        public override BuildStepResult ExecuteBuildStep()
-        {
-            var result = new BuildStepResult(true, null);
-            try
-            {
-                SQLServer.EnsureConnection();
+		public override BuildStepResult ExecuteBuildStep()
+		{
+			var result = new BuildStepResult(true, null);
+			try
+			{
+				SQLServer.EnsureConnection();
 
-                // Try and create the main database first. Without it, nothing works. Then
-                // we try and create any supporting databases.
-                bool creationOk = CreateDatabase(Configuration.MainDatabaseName);
-                if (creationOk)
-                {
-                    Logger.LogMessage("Main Database {0} created successfully. Attempting to create supporting databases (if any specified)", Configuration.MainDatabaseName);
-                    if (Configuration.SupportingDatabaseNames.Length > 0)
-                    {
-                        foreach (var db in Configuration.SupportingDatabaseNames)
-                        {
-                            creationOk = CreateDatabase(db);
-                            if (!creationOk)
-                {
-                                Logger.LogMessage("Supporting database {0} was NOT created successfully.", db);
-                                break;
-                }
-                else
-                {
-								if (!Configuration.PackageOnly)
+				// Try and create the main database first. Without it, nothing works. Then
+				// we try and create any supporting databases.
+				bool creationOk = CreateDatabase(Configuration.MainDatabaseName);
+				if (creationOk)
+				{
+					Logger.LogMessage("Main Database {0} created successfully. Attempting to create supporting databases (if any specified)", Configuration.MainDatabaseName);
+					if (Configuration.SupportingDatabaseNames.Length > 0)
+					{
+						foreach (var db in Configuration.SupportingDatabaseNames)
+						{
+							if (!string.IsNullOrEmpty(db))
+							{
+								creationOk = CreateDatabase(db);
+								if (!creationOk)
 								{
-									Logger.LogMessage("Supporting database {0} created successfully.", db);
+									Logger.LogMessage("Supporting database {0} was NOT created successfully.", db);
+									break;
 								}
-                            }
-                        }
-                    }
+								else
+								{
+									if (!Configuration.PackageOnly)
+									{
+										Logger.LogMessage("Supporting database {0} created successfully.", db);
+									}
+								}
+							}
+						}
+					}
 
-                }
+				}
 
-                if (creationOk)
-                {
-                    Logger.LogMessage("All database were created successfully.");
-                    result.Successful = true;
-                    result.CanContinue = true;
-                }
-                    else
-                    {
-                        result.Successful = false;
-                        result.CanContinue = false;
-                    result.ResultDetail = "Unable to create necessary databases.";
-                }
-            }
-            catch (Exception ex)
-            {
-                result.Successful = false;
-                result.CanContinue = false;
-                result.ResultDetail = ex.GetFullExceptionMessages();
-            }
+				if (creationOk)
+				{
+					Logger.LogMessage("All database were created successfully.");
+					result.Successful = true;
+					result.CanContinue = true;
+				}
+				else
+				{
+					result.Successful = false;
+					result.CanContinue = false;
+					result.ResultDetail = "Unable to create necessary databases.";
+				}
+			}
+			catch (Exception ex)
+			{
+				result.Successful = false;
+				result.CanContinue = false;
+				result.ResultDetail = ex.GetFullExceptionMessages();
+			}
 
-            return result;
-        }
+			return result;
+		}
 
-        public override string Name
-        {
-            get { return "Database Creation"; }
-        }
+		public override string Name
+		{
+			get { return "Database Creation"; }
+		}
 
-        private bool CreateDatabase(string databaseName)
-        {
+		private bool CreateDatabase(string databaseName)
+		{
 			if (Configuration.PackageOnly)
 			{
 				Logger.LogMessage("In PackageaOnly Mode. No need to create.", databaseName);
 				return true;
 			}
 
-        	if (SQLServer.DatabaseServer.Databases.Contains(databaseName))
-            {
-                Logger.LogMessage("Database {0} already exists. No need to create.", databaseName);
-                return true;
-            }
-            Logger.LogMessage("Database {0} not found. Attempting to create.", databaseName);
-            var dbToCreate = new Database(SQLServer.DatabaseServer, databaseName);
-            dbToCreate.Create();
-            SQLServer.DatabaseServer.Databases.Refresh();
-            return SQLServer.DatabaseServer.Databases.Contains(databaseName);
-        }
-    }
+			if (SQLServer.DatabaseServer.Databases.Contains(databaseName))
+			{
+				Logger.LogMessage("Database {0} already exists. No need to create.", databaseName);
+				return true;
+			}
+			Logger.LogMessage("Database {0} not found. Attempting to create.", databaseName);
+			var dbToCreate = new Database(SQLServer.DatabaseServer, databaseName);
+			dbToCreate.Create();
+			SQLServer.DatabaseServer.Databases.Refresh();
+			return SQLServer.DatabaseServer.Databases.Contains(databaseName);
+		}
+	}
 }
